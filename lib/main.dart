e6a1f14b9124606ef5b8f0b51412f98254c1a46a
Victor Leao <leaovic@gmail.com>
2025-11-02 14:17:39 -0300

    wip stripe

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,32 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart'; // Contains kIsWeb
 import 'package:lavajato/firebase_options.dart';
 import 'package:lavajato/services/auth_gate.dart';
-import 'package:lavajato/stripe_keys.dart';
+import 'package:lavajato/stripe_keys.dart'; // Assumed to contain stripePublishableKey
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // NOTE: You need to configure Firebase for your project.
-  // You can generate the configuration file by following the instructions here:
-  // https://firebase.google.com/docs/flutter/setup
+  
+  // 1. Initialize Firebase, conditionally using web options if running in a browser.
   await Firebase.initializeApp(
     options: kIsWeb ? DefaultFirebaseOptions.web : DefaultFirebaseOptions.currentPlatform,
   );
 
-  // Initialize Stripe
-  Stripe.publishableKey = stripePublishableKey;
-  await Stripe.instance.applySettings();
+  // 2. Initialize Stripe
+  // The 'flutter_stripe' package relies on platform-specific code (using dart:io/Platform)
+  // for initialization on mobile/desktop. This causes a crash on Flutter Web.
+  // We must wrap the initialization logic in an 'if (!kIsWeb)' check to prevent the crash.
+  if (!kIsWeb) {
+    // This block runs ONLY on mobile/desktop.
+    Stripe.publishableKey = stripePublishableKey;
+    await Stripe.instance.applySettings();
+  }
+  
+  // NOTE FOR WEB: On Flutter Web, the Stripe keys should also be configured 
+  // by ensuring the Stripe SDK is loaded via a <script> tag in your index.html file.
+  // The flutter_stripe package then bridges to the browser SDK automatically.
 
   runApp(const MyApp());
 }
@@ -34,8 +43,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      // New localization settings
-      localizationsDelegates: [
+      // Localization settings for Brazilian Portuguese
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
